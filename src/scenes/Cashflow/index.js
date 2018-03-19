723409c38850b76d334c90c3900603da47d4437a
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-03-19 02:17:02 +0100

    Implement get balance per day

--- src/scenes/Cashflow/index.js
+++ src/scenes/Cashflow/index.js
@@ -1,7 +1,20 @@
 import React, { PureComponent } from 'react';
+import { getBalancePerDay } from '../../tasks/getBalancePerDay';
+import { transactions } from '../../services/data/transactions';
 
 export class Cashflow extends PureComponent {
   render() {
-    return <h1>Cashflow</h1>;
+    const balancePerDay = getBalancePerDay(transactions, 3000);
+    return (
+      <div>
+        <h1>Cashflow</h1>
+        {Object.keys(balancePerDay).map(day => (
+          <p key={day}>
+            <b>{day}:</b>
+            {balancePerDay[day]}
+          </p>
+        ))}
+      </div>
+    );
   }
 }
--- src/tasks/getBalancePerDay/index.js
+++ src/tasks/getBalancePerDay/index.js
@@ -1,3 +1,27 @@
+// you must not mutate transactinsData
 export function getBalancePerDay(transactionsData, initialBalance = 0) {
+  const dayRecord = transactionsData.reduce((acc, transaction) => {
+    const date = new Date(transaction.date);
+    date.setHours(0, 0, 0, 0);
+    return (acc = {
+      ...acc,
+      [date]: add(acc[date], transaction.amount)
+    });
+  }, {});
+  return Object.keys(dayRecord).reduce((acc, day, i) => {
+    if (i === 0) {
+      return {
+        ...acc,
+        [day]: add(initialBalance, dayRecord[day])
+      };
+    }
+    return {
+      ...acc,
+      [day]: add(acc[Object.keys(dayRecord)[i - 1]], dayRecord[day])
+    };
+  }, {});
+}
 
+export function add(x = 0, y = 0) {
+  return (x * 100 + y * 100) / 100;
 }
